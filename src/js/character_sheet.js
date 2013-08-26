71957cf3537cb84c65a0dd60c8151112adc7c28d
Nicholas Steicke <nicholas.steicke@narthollis.net>
2013-08-27 00:33:17 +0930

    Verify/Retreive character listings for key/vcode combo and present the list to the user

--- src/js/character_sheet.js
+++ src/js/character_sheet.js
@@ -10,27 +10,27 @@
         var form = $('<form id="api_form_' + form_id + '"></form>');
 
         var name_html = '<div class="form-group">';
-        name_html+= '<label for="api_name_' + form_id + '">Name</label>';
+        name_html+= '<label for="api_name_' + form_id + '" class="control-label">API Label</label>';
         name_html+= '<input type="text" class="form-control" id="api_name_' + form_id + '" name="name" placeholder="Enter Name to identify this API Key">';
         name_html+= '</div>';
         name_html = $(name_html);
         form.append(name_html);
 
         var apikey_html = '<div class="form-group">';
-        apikey_html+= '<label for="api_key_' + form_id + '">API Key</label>';
+        apikey_html+= '<label for="api_key_' + form_id + '" class="control-label">API Key</label>';
         apikey_html+= '<input type="text" class="form-control" id="api_key_' + form_id + '" name="key" placeholder="Enter the API Key">';
         apikey_html+= '</div>';
         apikey_html = $(apikey_html);
         form.append(apikey_html);
 
         var vcode_html = '<div class="form-group">';
-        vcode_html+= '<label for="api_vcode_' + form_id + '">vCode</label>';
+        vcode_html+= '<label for="api_vcode_' + form_id + '" class="control-label">vCode</label>';
         vcode_html+= '<input type="text" class="form-control" id="api_vcode_' + form_id + '" name="vcode" placeholder="Enter the vCode">';
         vcode_html+= '</div>';
         vcode_html = $(vcode_html);
         form.append(vcode_html);
 
-        var button = $('<button type="submit" class="btn btn-primary pull-right" data-loading-text="Verifying...">Verify and Save</button>');
+        var button = $('<button type="submit" class="btn btn-primary pull-right" data-loading-text="Verifying...">Verify</button>');
         form.append(button);
 
         form.bind('submit', verifyApiKey);
@@ -39,6 +39,9 @@
     };
 
     var verifyApiKey = function( event ) {
+        event.stopPropagation();
+        event.preventDefault();
+
         var form = $(event.currentTarget);
 
         form.find('input').prop('disabled', true);
@@ -48,13 +51,69 @@
         var key = form.find('input[name=key]').val()
         var vcode = form.find('input[name=vcode]').val()
 
-        $.fn.eve_api('account/APIKeyInfo.xml.aspx', {'keyID': key, 'vCode': vcode}, function(xml) { apiKeyVerificationResault(xml, form); });
+        $.fn.eve_api(
+            'account/APIKeyInfo.xml',
+            {'keyID': key, 'vCode': vcode},
+            function(xml) { apiKeyVerificationResault(xml, form); },
+            function(jqXHR, status, responseText) { apiKeyVerificationFailure(form); }
+        );
 
         return false;
     };
 
+    var apiKeyVerificationFailure = function(form) {
+        form.find('input').prop('disabled', false);
+        form.find('button').button('reset');
+
+        $(form.find('input[name=key]').parent()).addClass('has-error');
+        $(form.find('input[name=vcode]').parent()).addClass('has-error');
+    };
+
     var apiKeyVerificationResault = function(xml, form) {
-        console.log(xml, form);
+        var name = form.find('input[name=name]').val()
+        var key = form.find('input[name=key]').val()
+        var vcode = form.find('input[name=vcode]').val()
+
+        $(form.find('input[name=key]').parent()).remove();
+        $(form.find('input[name=vcode]').parent()).remove();
+        form.find('button').remove();
+
+        var rows = document.evaluate(
+                '/eveapi/result/key/rowset/row',
+                xml,
+                null,
+                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                null
+            );
+
+        for ( var i=0 ; i < rows.snapshotLength; i++ ) {
+            var charID = $.trim($(rows.snapshotItem(i)).attr('characterID'));
+            var charName = $.trim($(rows.snapshotItem(i)).attr('characterName'));
+            var corpName = $.trim($(rows.snapshotItem(i)).attr('corporationName'));
+
+            var checkbox = '<div class="checkbox">';
+            checkbox+='<label for="char_' + charID +'">';
+            checkbox+='<input type="checkbox" id="char_' + charID +'" value="' + charID + '">';
+            checkbox+= charName + ' (' + corpName + ')'
+            checkbox+='</label></div>';
+
+            form.append($(checkbox));
+        }
+
+        var button = $('<button type="submit" class="btn btn-primary pull-right" data-loading-text="Saving...">Save</button>');
+        form.append(button);
+
+        form.unbind('submit');
+        form.bind('submit', saveCharacterAndAPI);
+    };
+
+    var saveCharacterAndAPI = function( event ) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        //TODO: Upto here marker
+
+        var form = $(event.currentTarget);
     };
 
     var drawExistingKey = function( key ) {
