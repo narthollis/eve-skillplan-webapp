910e57b2f5b4fc0965f35f8ce56bc2daa0321633
Nicholas Steicke <nicholas.steicke@narthollis.net>
2013-08-29 16:35:06 +0930

    Various updates and fixes the the API conviance method

--- src/js/eve_api.js
+++ src/js/eve_api.js
@@ -1,32 +1,43 @@
 "use strict";
 
 (function( $ ) {
-    var complete = function( path, param_str, callback, error_callback, xml, status, jqXHR ) {
+    var complete = function( path, settings, xml, status, jqXHR ) {
         if (status != "success") {
-            error_callback(xml, status, jqXHR);
+            if (settings['error_callback'])
+                settings['error_callback'](xml, status, jqXHR);
+
         } else {
             var expires = xml.getElementsByTagName('cachedUntil')[0].childNodes[0].data;
 
-            var match = expires.match(/^(\d+)-(\d+)-(\d+) (\d+)\:(\d+)\:(\d+)$/);
-            var expires = new Date(match[1], match[2] - 1, match[3], match[4], match[5], match[6]).getTime()/1000;
+            //var match = expires.match(/^(\d+)-(\d+)-(\d+) (\d+)\:(\d+)\:(\d+)$/);
+            //var expires = new Date(match[1], match[2] - 1, match[3], match[4], match[5], match[6]).getTime()/1000;
+            
+            expires = Math.floor(new Date(expires + '+UTC').getTime()/1000);
 
-            window.localStorage['_eve_api_cache_' + path + param_str] = JSON.stringify({
+            window.localStorage['_eve_api_cache_' + path + settings['params']] = JSON.stringify({
                 'expires': expires,
                 'responseText': jqXHR.responseText.toString()
             });
 
-            callback(xml);
+            if (settings['callback']) settings['callback'](xml);
         }
     };
 
-    $.fn.eve_api = function( path, params, callback, error_callback ) {
-        var param_str = JSON.stringify(params);
+    $.fn.eve_api = function(path, options) {
+        var settings = $.extend({
+            'params': {},
+            'callback': false,
+            'error_callback': false,
+            'return_or_fail': false
+        }, options);
 
-        if (window.localStorage.hasOwnProperty('_eve_api_cache_' + path + param_str)) {
-            var response = JSON.parse(window.localStorage['_eve_api_cache_' + path + param_str]);
+        settings['params'] = JSON.stringify(settings['params']);
 
-            if (response['expires'] < parseInt(new Date().getTime()/1000)) {
-                console.log('cache');
+        if (window.localStorage.hasOwnProperty('_eve_api_cache_' + path + settings['params'])) {
+            var response = JSON.parse(window.localStorage['_eve_api_cache_' + path + settings['params']]);
+
+            if (response['expires'] > Math.floor(new Date().getTime()/1000)) {
+                console.log('from cache');
 
                 var xml;
                 if (window.DOMParser) {
@@ -38,19 +49,25 @@
                     xml.loadXML(response['responseText']); 
                 }
 
-                callback(xml);
+                if (settings['return_or_fail']) return xml;
+
+                settings['callback'](xml);
                 return;
+            } else {
+                delete window.localStorage['_eve_api_cache_' + path + settings['params']]
             }
         }
 
+        if (settings['return_or_fail']) return null;
+
         //$.ajax('https://api.eveonline.com/' + path, {
         $.ajax('https://eveapinarth.appspot.com/' + path, {
-            'data': params,
+            'data': JSON.parse(settings['params']),
             /*'dataType': 'text',*/
             'type': 'POST'
         }).always(
             function(xml, status, jqXHR) {
-                complete(path, param_str, callback, error_callback, xml, status, jqXHR);
+                complete(path, settings, xml, status, jqXHR);
             }
         );
     };
